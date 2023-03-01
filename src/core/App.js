57b4f3e3333d2c596acbee0610ae7725136cb2ab
maxtermax <esnene02@gmail.com>
2023-03-01 08:23:19 -0500

    Removing repeated item

--- src/core/App.js
+++ src/core/App.js
@@ -22,13 +22,6 @@ const sneakerList = [
     name: 'Adidas Forum',
     price: '200'
   },
-  {
-    id: '3',
-    image: '/assets/images/addidas.webp',
-    description: 'Bad Bunny Forum Buckle Low sneakers',
-    name: 'Adidas Forum',
-    price: '200'
-  }
 ]
 
 const productsStore = new Map();
